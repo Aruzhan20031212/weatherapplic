9d534c66e821d1e1588bf241a21454f66fe6a08e
Aruzhan20031212 <148435400+Aruzhan20031212@users.noreply.github.com>
2023-12-19 21:19:11 +0600

    Add files via upload

---
+++ info.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Info = () => (
+<div>
+                <p><h3>Check the weather in your city!</h3></p>
+                
+                
+            </div>
+)
+
+export default Info;
